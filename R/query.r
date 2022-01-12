36c36ce69d436c34fd51848b429a8554a7ae52a9
Tom Palmer <remlapmot@hotmail.com>
2022-01-12 18:41:58 +0000

    Fix F to FALSE and long lines

--- R/query.r
+++ R/query.r
@@ -335,7 +335,8 @@ query_rsid_bcftools <- function(rsid, vcffile, id=NULL)
 	}
 	id <- paste(id, collapse=",")
 	tmp <- tempfile()
-	utils::write.table(unique(rsid), file=paste0(tmp, ".snplist"), row=F, col=F, qu=F)
+	utils::write.table(unique(rsid), file=paste0(tmp, ".snplist"), 
+	                   row=FALSE, col=FALSE, qu=FALSE)
 	cmd <- sprintf("%s view -s %s -i'ID=@%s.snplist' %s > %s.vcf", bcftools, id, tmp, vcffile, tmp)
 	system(cmd)
 	o <- VariantAnnotation::readVcf(paste0(tmp, ".vcf"))
@@ -390,7 +391,9 @@ query_chrompos_bcftools <- function(chrompos, vcffile, id=NULL)
 	chrompos <- parse_chrompos(chrompos)
 	chrompos %>% as.data.frame
 	tmp <- tempfile()
-	utils::write.table(as.data.frame(chrompos)[,1:3], file=paste0(tmp, ".snplist"), sep="\t", row=F, col=F, qu=F)
+	utils::write.table(as.data.frame(chrompos)[,1:3], 
+	                   file=paste0(tmp, ".snplist"), sep="\t", 
+	                   row=FALSE, col=FALSE, qu=FALSE)
 
 	cmd <- sprintf(paste0("%s view %s -R %s.snplist %s > %s.vcf"), bcftools, idclause, tmp, vcffile, tmp)
 	system(cmd)
